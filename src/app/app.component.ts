3f6868d29f9f23cd6b5ebb181eda317931250650
phattharaphorn <64010659@kmitl.ac.th>
2024-04-09 08:43:24 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'my-app';
+  public title = "My Portfolio";
+
+  public name = "Angular";
+  public topic = "Data Binding";
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component'; // Import here
+
+@NgModule({
+  imports: [
+    AppComponent // Import the standalone component
+  ],
+  // ...
+})
+export class AppModule {}
